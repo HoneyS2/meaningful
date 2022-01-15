1fd6390ca3d4139aca039f7a88b9f002e74f66b9
HoneyS2 <Symbiote.Sim@gmail.com>
2022-01-15 22:48:24 +0900

    Add longestCommonPrefix

---
+++ algorithm/python/LeetCode/longestCommonPrefix.py
@@ -0,0 +1,13 @@
+strs = ["flower","flow","flight"]
+
+if len(strs) == 0:
+    print("")
+    exit()
+
+prefix = strs[0]
+for str in strs:
+    while(str.find(prefix) != 0):
+        prefix = prefix[0:len(prefix)-1]
+        if len(prefix) == 0:
+            print("")
+print(prefix)
